88479008f4222b1b6963683914c0abeedaae68a5
Z-kamili <zakariakamili97@gmail.com>
2022-10-19 00:30:10 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,87 @@
+<html>
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<head>
+<title>SUPERMAN ZIKO</title>
+<meta charset="utf-8">
+<style>
+@font-face { font-family:"TopazPlus"; src: url(topazplus500.html);}
+      body { color: #dddddd;
+             background-color: #0c0d10; 
+             font-size: 16px;
+             line-height: 16px;
+             margin: 0 auto;
+             padding: 1em 0 1em 0;
+           }
+       pre { font-family:"TopazPlus", "Lucida Console", monospace, Monaco; }
+         a { color: #98f5ff; text-decoration: none; }
+       iky { color: #74ee15; }
+       ty  { font-size: 80px;color: #98f5ff;font-family: "TopazPlus", "Lucida Console", monospace, Monaco;  }
+         y { color: #ffa298; }
+        gy { color: #f5ff98; }
+         w { color: #ffffff; }
+        gr { color: #b5bd68; }
+         r { color: #f22e2e; }
+       pnk { color: #f4b6e5; }
+        bb { color: #5cc2ca; }
+       prp { color: #7a80a2; }
+         l { color: #c5c8c6; }
+        lg { color: #9de9a9; }
+</style>
+<link rel=“shortcut icon” href="favicon.ico" type=“image/x-icon”>
+<link rel=“icon” href="favicon.ico" type=“image/x-icon”>
+</head>
+<body><center>
+<div style="display: inline-block; text-align: left;"><pre>
+
+
+                           <ty>ZIKO</ty>
+
+|                                                                              |
+| Links: <a href="https://twitter.com/zakariakamili97">Twitter</a> | <a href="https://www.facebook.com/zakaria.kamili.52">Facebook</a>                                                    |
+|                                                                              |    
+| Hi, My name is Zakaria Kamili and I'm a FULL STACK Developer,I love learning |   
+| and teaching any tech related to software engineering                        |
+| and my big dream is to create my own product that can change the world.      |
+|                                                                              |
+|                                                                              |
+| My professional interests:                                                   |
+|  - PHP Especially Laravel                                                    |
+|  - Open Source Project                                                       |
+|  - Networking                                                                |
+|  - Social Engineering                                                        |
+|                                                                              |
+| **First place in INJAZ Hackathon**                                           |
+|                                                                              |                                         
+|                                                                              |
+| Happy Coding!                                                                |
+|                                                                              |
+|                                                                              |
+|<y>-- <w>My Accounts </w> ------------------------------------------------------</y>        |
+|                                                                              |
+| <gy>::</gy> <a href="https://www.youtube.com/channel/UCnNcvF8WYNnAfcMIRrlaDQw/featured"> Youtube channel    <pnk>[YouTube Link]</pnk> </a>                                       |
+|                                                                              |
+| <gy>::</gy> <a href="https://www.youtube.com/channel/UCnNcvF8WYNnAfcMIRrlaDQw/featured"> Linkedin Account   <pnk>[Linkedin Link]</pnk> </a>                                      |
+|                                                                              |
+|<y>-- <w>My favorite sources  </w> ---------------------------------------------</y>        |
+|                                                                              |
+| <gy>::</gy> <a href="https://laravel.com/">Laravel  <pnk>[Laravel Website]</pnk> </a>                                               |
+|                                                                                         
+| <gy>::</gy> <a href="https://www.youtube.com/watch?v=127ng7botO4&t=654s&ab_channel=OfferZenOrigins">Laravel Origins</a> <pnk>[PHP Documentary]</pnk>                                         |
+|                                                                              |
+| <gy>::</gy> <a href="https://www.youtube.com/watch?v=SEwXlG9jxf0&list=PLfrhw4n7bwKM5SrN0AlqHhavMgjJXTqGS&index=6&ab_channel=OfferZenOrigins">Laracasts<pnk> [The story of Laracasts feat]</pnk></a>                                   |
+|                                                                              |
+| <gy>::</gy> <a href="https://www.youtube.com/watch?v=ObUgYDn1zZ0&t=3352s&ab_channel=DavidBombal">Hacker hunting with Wireshark<pnk> [YouTube Link]</pnk></a>                              |
+|                                                                              |
+| <gy>::</gy> <a href="https://www.youtube.com/watch?v=kVSCFBuh9xs&ab_channel=DavidBombal">Is this the future of the Internet? UDP Deep Dive </a><pnk>[YouTube Link]</pnk>          |
+|                                                                              |
+|                                                                              |
+|----------------------------zakariakamili97@gmail.com-------------------------|
+:<y>______________________________________________________________________________</y>:
+:<y>------------------------------------------------------------------------------</y>:
+<gy></gy>
+</pre>
+</div>
+</center>
+</body>
+
+</html>
